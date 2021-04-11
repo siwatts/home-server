bf5a4ea3936a329be7032d296777f03b2d197e23
Simon Watts <siwatts15@outlook.com>
2021-04-11 16:00:22 +0000

    Enhance install script
    - Autocompare and remove .bak files, ssh config
    - Add colour

--- install-client.sh
+++ install-client.sh
@@ -11,7 +11,9 @@ SSH_IDPUB_LO="$HOME/.ssh/id_rsa.pub"
 
 # Quickly check we are in the right place
 if [ ! -f "$SSH_AUTHK_RE" ]; then
+    tput setaf 1; tput bold
     echo "ERROR: Cannot see file '$SSH_AUTHK_RE', please run script from root of repository"
+    tput sgr0
     exit 0
 fi
 
@@ -35,8 +37,10 @@ if [ -f $SSH_IDPUB_LO ]; then
     echo "Appending to list of authorized keys..."
     cat "$SSH_IDPUB_LO" >> "$SSH_AUTHK_RE"
 else
+    tput setaf 1; tput bold
     echo "ERROR: Found no SSH key '$SSH_IDPUB_LO'"
     echo "Aborting"
+    tput sgr0
     exit 0
 fi
 
@@ -45,9 +49,10 @@ read -r -p 'Import authorized_keys? (y/[N]): ' response
 case "$response" in
     [yY][eE][sS]|[yY])
         if [ -f "$SSH_AUTHK_LO" ]; then
+            tput setaf 5
             echo "WARNING: Found existing '$SSH_AUTHK_LO' file, moving to '$SSH_AUTHK_LO.bak'"
             echo "         Please delete or merge these files"
-            echo ""
+            tput sgr0
             mv "$SSH_AUTHK_LO" "$SSH_AUTHK_LO.bak"
         fi
         echo "Soft-linking '$SSH_AUTHK_RE'..."
@@ -57,11 +62,12 @@ case "$response" in
         ;;
 esac
 
-echo "Importing SSH hosts file '$SSH_CONFIG_RE'"
+echo "Importing SSH hosts file '$SSH_CONFIG_RE'..."
 if [ -f $SSH_CONFIG_LO ]; then
+    tput setaf 5
     echo "WARNING: Found existing '$SSH_CONFIG_LO' file, moving to '$SSH_CONFIG_LO.bak'"
     echo "         Please delete or merge these files"
-    echo ""
+    tput sgr0
     mv "$SSH_CONFIG_LO" "$SSH_CONFIG_LO.bak"
 fi
 echo "Soft-linking SSH hosts file '$SSH_CONFIG_RE'..."
@@ -70,6 +76,32 @@ ln -s "$(pwd)/$SSH_CONFIG_RE" "$SSH_CONFIG_LO"
 echo "Fixing some file permissions..."
 chmod 600 "$SSH_AUTHK_LO"
 chmod 664 "$SSH_CONFIG_LO"
+
+if [ -f "$SSH_CONFIG_LO.bak" ]; then
+    echo "Opening files for comparison in vimdiff : '$SSH_CONFIG_LO' -> '$SSH_CONFIG_LO.bak'..."
+    echo "  '$SSH_CONFIG_LO.bak' will be deleted on successful exit code. Exit with ':cq' to avoid this."
+    read -r -p "Compare files? (y/[N]): " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            vimdiff "$SSH_CONFIG_LO" "$SSH_CONFIG_LO.bak" && rm "$SSH_CONFIG_LO.bak"
+            ;;
+        *)
+            ;;
+    esac
+fi
+
+if [ -f "$SSH_AUTHK_LO.bak" ]; then
+    echo "Opening files for comparison in vimdiff : '$SSH_AUTHK_LO' -> '$SSH_AUTHK_LO.bak'..."
+    echo "  '$SSH_AUTHK_LO.bak' will be deleted on successful exit code. Exit with ':cq' to avoid this."
+    read -r -p "Compare files? (y/[N]): " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            vimdiff "$SSH_AUTHK_LO" "$SSH_AUTHK_LO.bak" && rm "$SSH_AUTHK_LO.bak"
+            ;;
+        *)
+            ;;
+    esac
+fi
 
 echo ""
 echo "Done"
